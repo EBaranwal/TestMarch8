7b47302c8251b2456ecc8b7752db6385c73670f6
Baranwal <ekta.baranwal@accenture.com>
2018-03-08 13:03:25 +0530

    my 2nd commit to print wishes 10 times

--- src/test/Wishes.java
+++ src/test/Wishes.java
@@ -4,6 +4,8 @@ public class Wishes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		for( int i=0; i<10;i++)
 		
 		System.out.println("Happy Woman's day to all ");
 
